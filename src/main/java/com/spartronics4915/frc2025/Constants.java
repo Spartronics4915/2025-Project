d74ddd44b9ebdc74707c674f5ce4b684bcb654c1
otter502 <115671877+otter502@users.noreply.github.com>
2024-12-04 17:59:14 -0800

    Change motor ID to placeholder

--- src/main/java/com/spartronics4915/frc2025/Constants.java
+++ src/main/java/com/spartronics4915/frc2025/Constants.java
@@ -7,6 +7,6 @@ import com.spartronics4915.frc2025.Util.MotorConstants;
 public class Constants {
     public static final class IntakeWristConstants { // [ ] Intake Wrist constants
         public static final MotorConstants kMotorConstants = new MotorConstants(
-            19, MotorType.kBrushless, false, IdleMode.kBrake, 40);
+            0, MotorType.kBrushless, false, IdleMode.kBrake, 40);
     }
 }
