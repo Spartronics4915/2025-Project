7967ed5f0f2ae80d54752b1845ef1de90be0b999
Pointygreenskies <gnomeceratops@gmail.com>
2024-12-11 19:55:28 -0800

    Added CANSparkMax to IntakeWrist

--- src/main/java/com/spartronics4915/frc2025/subsystems/IntakeWrist.java
+++ src/main/java/com/spartronics4915/frc2025/subsystems/IntakeWrist.java
@@ -1,7 +1,10 @@
 package com.spartronics4915.frc2025.subsystems;
-
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import com.revrobotics.CANSparkMax;
+import com.revrobotics.CANSparkLowLevel.MotorType;
+
 
 public class IntakeWrist extends SubsystemBase {
+    public final CANSparkMax mMotor;
     
 }
